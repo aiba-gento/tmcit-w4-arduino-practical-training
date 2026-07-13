de0749cdce21797ec8a39ed5a7711b22169981e9
aiba-gento <8gn24gn25@gmail.com>
2026-07-13 09:41:32 +0900

    feat:pattern modified

--- src/main.cpp
+++ src/main.cpp
@@ -15,19 +15,21 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(LED1, HIGH);
-  delay(1000);
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED2, HIGH);
-  delay(1000);
-  digitalWrite(LED2, LOW);
-  digitalWrite(LED3, HIGH);
-  delay(1000);
-  digitalWrite(LED3, LOW);
-  digitalWrite(LED4, HIGH);
-  delay(1000);
-  digitalWrite(LED4, LOW);
-  digitalWrite(LED5, HIGH);
-  delay(1000);
   digitalWrite(LED5, LOW);
+  digitalWrite(LED1, HIGH);
+  delay(500);
+  digitalWrite(LED5, LOW);
+  digitalWrite(LED2, HIGH);
+  delay(500);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED3, HIGH);
+  delay(500);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED4, HIGH);
+  delay(500);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED5, HIGH);
+  delay(500);
+  digitalWrite(LED4, LOW);
+  delay(500);
 }
