d26e658f041f9b75a9e9c05f198f8ccca7592c9c
aiba-gento <8gn24gn25@gmail.com>
2026-07-13 10:00:26 +0900

    feat:sequence control

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,30 @@
 #include <Arduino.h>
 
-constexpr int LED1 = 13;
-constexpr int LED2 = 9;
-constexpr int LED3 = 10;
-constexpr int LED4 = 11;
-constexpr int LED5 = 12;
-
 constexpr int MOTOR_DRIVER_IN1 = 5;
 constexpr int MOTOR_DRIVER_IN2 = 6;
 
+void stop_motor_2_steps() {
+  digitalWrite(MOTOR_DRIVER_IN1, LOW);
+  digitalWrite(MOTOR_DRIVER_IN2, LOW);
+  delay(500);
+  digitalWrite(MOTOR_DRIVER_IN1, HIGH);
+  digitalWrite(MOTOR_DRIVER_IN2, HIGH);
+  delay(500);
+
+}
+
 void setup() {
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-  pinMode(LED5, OUTPUT);
   pinMode(MOTOR_DRIVER_IN1, OUTPUT);
   pinMode(MOTOR_DRIVER_IN2, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED1, HIGH);
-  delay(500);
-  digitalWrite(LED1, LOW);
-  delay(500);
+  digitalWrite(MOTOR_DRIVER_IN1, HIGH);
+  digitalWrite(MOTOR_DRIVER_IN2, LOW);
+  delay(5000);
+  stop_motor_2_steps();
+  digitalWrite(MOTOR_DRIVER_IN1, LOW);
+  digitalWrite(MOTOR_DRIVER_IN2, HIGH);
+  delay(5000);
+  stop_motor_2_steps();
 }
