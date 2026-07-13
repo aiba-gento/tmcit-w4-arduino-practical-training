8b123ba16ede8c6a41994e53e842736f2bb6ac96
aiba-gento <8gn24gn25@gmail.com>
2026-07-13 10:45:27 +0900

    fix:delay time

--- src/main.cpp
+++ src/main.cpp
@@ -36,11 +36,8 @@ void setup() {
 }
 
 void loop() {
-  // 半固定抵抗の値の応じて２週する時間を変化し丁度いいタイムに変更する
-  int vr_value = analogRead(VR1);
-  int loop_time = map(vr_value, 0, 1023, 10000, 100000);
   run_motor(1);
-  delay(loop_time);
+  delay(50000);
   run_motor(0);
-  delay(1000);
+  delay(3000);
 }
