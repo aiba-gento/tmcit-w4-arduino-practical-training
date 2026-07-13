7884be5c68eeaf7ff0242bc5cb96a679d68ab1ed
aiba-gento <8gn24gn25@gmail.com>
2026-07-13 09:28:19 +0900

    feat:LED blink

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,28 @@ constexpr int LED3 = 10;
 constexpr int LED4 = 11;
 constexpr int LED5 = 12;
 
-constexpr int MOTOR_DRIVER_IN1 = 5;
-constexpr int MOTOR_DRIVER_IN2 = 6;
-
 void setup() {
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
   pinMode(LED4, OUTPUT);
   pinMode(LED5, OUTPUT);
-  pinMode(MOTOR_DRIVER_IN1, OUTPUT);
-  pinMode(MOTOR_DRIVER_IN2, OUTPUT);
 }
 
 void loop() {
   digitalWrite(LED1, HIGH);
-  delay(500);
+  delay(1000);
   digitalWrite(LED1, LOW);
-  delay(500);
+  digitalWrite(LED2, HIGH);
+  delay(1000);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  delay(1000);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, HIGH);
+  delay(1000);
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED5, HIGH);
+  delay(1000);
+  digitalWrite(LED5, LOW);
 }
