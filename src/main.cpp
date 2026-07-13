fa2782aced385b6552e75146ff5cffba1102cfd7
aiba-gento <8gn24gn25@gmail.com>
2026-07-13 10:20:01 +0900

    feat:adjustable-2loop-times

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,21 @@ constexpr int LED5 = 12;
 constexpr int MOTOR_DRIVER_IN1 = 5;
 constexpr int MOTOR_DRIVER_IN2 = 6;
 
+constexpr int VR1 = A0;
+
+void run_motor(int speed) {
+  if (speed > 0) {
+    digitalWrite(MOTOR_DRIVER_IN1, HIGH);
+    digitalWrite(MOTOR_DRIVER_IN2, LOW);
+  } else if (speed < 0) {
+    digitalWrite(MOTOR_DRIVER_IN1, LOW);
+    digitalWrite(MOTOR_DRIVER_IN2, HIGH);
+  } else {
+    digitalWrite(MOTOR_DRIVER_IN1, HIGH);
+    digitalWrite(MOTOR_DRIVER_IN2, HIGH);
+  }
+}
+
 void setup() {
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
@@ -17,11 +32,15 @@ void setup() {
   pinMode(LED5, OUTPUT);
   pinMode(MOTOR_DRIVER_IN1, OUTPUT);
   pinMode(MOTOR_DRIVER_IN2, OUTPUT);
+  pinMode(VR1, INPUT);
 }
 
 void loop() {
-  digitalWrite(LED1, HIGH);
-  delay(500);
-  digitalWrite(LED1, LOW);
-  delay(500);
+  // 半固定抵抗の値の応じて２週する時間を変化し丁度いいタイムに変更する
+  int vr_value = analogRead(VR1);
+  int loop_time = map(vr_value, 0, 1023, 10000, 100000);
+  run_motor(1);
+  delay(loop_time);
+  run_motor(0);
+  delay(1000);
 }
